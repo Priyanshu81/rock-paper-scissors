36a3edba5b1756872df7763cf92e58525032aa18
Priyanshu <mpriyanshu481@gmail.com>
2023-01-20 15:04:54 +0530

    complete the basic code

---
+++ javascript.js
@@ -0,0 +1,67 @@
+/*
+make a function that randomly returns one of rock,paper,scissors as computer(also applying uppercase for absolute equality)
+make a function that takes the input from the user(also applying uppercase here)
+make a case-insensitive comparison function between the return values of these 2 values
+return whether the computer or the computer won the round
+*/
+let n = prompt("How many rounds do you want to play?");
+for(let i = 0;i<n;i++){
+    console.log(getWinner(getComputerChoice(), getUserChoice()));
+}
+
+function getComputerChoice(){
+    let randomValue;
+    let randomInt = Math.floor(Math.random() * 3);
+    switch(randomInt){
+        case 0:
+            randomValue = "Rock";
+            break;
+        case 1:
+            randomValue = "Paper";
+            break;
+        case 2:
+            randomValue = "Scissors";
+            break;
+    }
+    console.log(`CPU: I choose ${randomValue}`);
+    return randomValue.toUpperCase(); 
+}
+
+function getUserChoice(){
+    let userChoice;
+    userChoice = prompt("Enter your choice:");
+    return userChoice.toUpperCase();    
+}
+
+function getWinner(ComputerChoice, UserChoice){
+    if(ComputerChoice === UserChoice){
+        return "It's a tie";
+    }
+    
+    if(UserChoice === "ROCK"){
+        if(ComputerChoice === "PAPER"){
+            return "You lost!";
+        }
+        if(ComputerChoice === "SCISSORS"){
+            return "You won!!";
+        }
+    }
+
+    if(UserChoice === "PAPER"){
+        if(ComputerChoice === "SCISSORS"){
+            return "You lost!";
+        }
+        if(ComputerChoice === "ROCK"){
+            return "You won!!";
+        }
+    }
+
+    if(UserChoice === "SCISSORS"){
+        if(ComputerChoice === "ROCK"){
+            return "You lost!";
+        }
+        if(ComputerChoice === "PAPER"){
+            return "You won!!";
+        }
+    }
+}
